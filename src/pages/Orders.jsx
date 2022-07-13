895ac3f5867aae88e33f0eb5f9f6f5c71d80b35e
Stephen <stvlley.amzn@gmail.com>
2022-07-12 20:54:40 -0400

    reportssssssssss

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 import { Header } from '../components';
 
 const Orders = () => {
+
+  const [reports, setReports] = useState([])
+    
   const editing = { allowDeleting: true, allowEditing: true };
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
